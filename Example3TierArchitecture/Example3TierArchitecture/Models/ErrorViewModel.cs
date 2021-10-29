cd231d36885e7df87169cfb0b8db6bd3716fdce9
sergeysud <sergeysud@wizardsdev.com>
2021-10-29 20:08:53 +0300

    create progect

---
+++ Example3TierArchitecture/Example3TierArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Example3TierArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
