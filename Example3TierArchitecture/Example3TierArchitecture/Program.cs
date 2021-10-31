12116a791c32aa2dcc1a9e255b47d0d86a4c31e5
sergeysud <sergeysud@wizardsdev.com>
2021-10-31 21:28:34 +0200

    add serilog

--- Example3TierArchitecture/Example3TierArchitecture/Program.cs
+++ Example3TierArchitecture/Example3TierArchitecture/Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataLayer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace Example3TierArchitecture
 {
@@ -13,11 +12,45 @@ namespace Example3TierArchitecture
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting up");
+                var host = CreateHostBuilder(args).Build();
+                // DB
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var db = services.GetRequiredService<EFDBContext>();
+
+                    // Migrate
+                    Log.Information("Appling DB Migrations");
+                    db.Database.Migrate();
+
+                    // Seed
+                    Log.Information("Seeding DB");
+                    SampleData.InitData(db);
+                }
+
+                Log.Information("Launching App");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
