611fea22b43c4317aa20e60a82254215d68a958e
sergeysud <sergeysud@wizardsdev.com>
2021-10-31 23:13:59 +0200

    add Views for viewing and editing

--- Example3TierArchitecture/BuisnessLayer/Implementations/EFDirectorysRepository.cs
+++ Example3TierArchitecture/BuisnessLayer/Implementations/EFDirectorysRepository.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using BuisnessLayer.Interfaces;
 using DataLayer;
 using DataLayer.Entityes;
---
+++ Example3TierArchitecture/Example3TierArchitecture/Controllers/PageController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuisnessLayer;
+using DataLayer.Enums;
+using PresentationLayer;
+using PresentationLayer.Models;
+
+namespace Example3TierArchitecture.Controllers
+{
+    public class PageController : Controller
+    {
+        private DataManager _datamanager;
+        private ServicesManager _servicesmanager;
+
+        public PageController(DataManager dataManager)
+        {
+            _datamanager = dataManager;
+            _servicesmanager = new ServicesManager(dataManager);
+        }
+        public IActionResult Index(int pageId, PageEnums.PageType pageType)
+        {
+            PageViewModel _viewModel;
+            switch (pageType)
+            {
+                case PageEnums.PageType.Directory: _viewModel = _servicesmanager.Directorys.DirectoryDBToViewModelById(pageId); break;
+                case PageEnums.PageType.Material: _viewModel = _servicesmanager.Materials.MaterialDBModelToView(pageId); break;
+                default: _viewModel = null; break;
+            }
+            ViewBag.PageType = pageType;
+            return View(_viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult PageEditor(int pageId, PageEnums.PageType pageType, int directoryId = 0)
+        {
+            PageEditModel _editModel;
+
+            switch (pageType)
+            {
+                case PageEnums.PageType.Directory:
+                    if (pageId != 0) _editModel = _servicesmanager.Directorys.GetDirectoryEdetModel(pageId);
+                    else _editModel = _servicesmanager.Directorys.CreateNewDirectoryEditModel();
+                    break;
+                case PageEnums.PageType.Material:
+                    if (pageId != 0) _editModel = _servicesmanager.Materials.GetMaterialEdetModel(pageId);
+                    else _editModel = _servicesmanager.Materials.CreateNewMaterialEditModel(directoryId);
+                    break;
+                default: _editModel = null; break;
+            }
+
+            ViewBag.PageType = pageType;
+            return View(_editModel);
+        }
+
+        [HttpPost]
+        public IActionResult SaveDirectory(DirectoryEditModel model)
+        {
+            _servicesmanager.Directorys.SaveDirectoryEditModelToDb(model);
+            return RedirectToAction("PageEditor", "Page", new { pageId = model.Id, pageType = PageEnums.PageType.Directory });
+        }
+
+        [HttpPost]
+        public IActionResult SaveMaterial(MaterialEditModel model)
+        {
+            _servicesmanager.Materials.SaveMaterialEditModelToDb(model);
+            return RedirectToAction("PageEditor", "Page", new { pageId = model.Id, pageType = PageEnums.PageType.Material });
+        }
+    }
+}
